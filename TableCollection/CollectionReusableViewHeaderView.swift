e7016baeea62f7788cec5649fdf85907062f1527
iyinraphael <iyinraphael@gmail.com>
2018-11-28 09:41:44 -0800

    created type fpr different controller

---
+++ TableCollection/CollectionReusableViewHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionReusableViewHeaderView.swift
+//  TableCollection
+//
+//  Created by Iyin Raphael on 11/28/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableViewHeaderView: UICollectionReusableView {
+        
+}
---
+++ TableCollection/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableCollection
+//
+//  Created by Iyin Raphael on 11/28/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
