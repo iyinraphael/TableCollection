dd928b881b02265ade2820663c16faf0265b54fb
iyinraphael <iyinraphael@gmail.com>
2018-11-28 10:12:48 -0800

    set up code for tableViewController

--- TableCollection/TableViewCell.swift
+++ TableCollection/TableViewCell.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    static let reuseIdentifier = "tableCell"
+    
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var swatchView: UIView!
+    
+    //static properties mean you can access themm through the class without having
+    //to create an instance of the class
 
 }
